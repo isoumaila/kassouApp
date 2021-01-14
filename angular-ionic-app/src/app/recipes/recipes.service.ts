dd6f0d150b8963ccce37b7a819ca50a7603ee9a9
isoumaila <abarham.soumaila@yahoo.com>
2021-01-14 22:14:48 +0100

    adding new pictures

--- angular-ionic-app/src/app/recipes/recipes.service.ts
+++ angular-ionic-app/src/app/recipes/recipes.service.ts
@@ -9,7 +9,7 @@ export class RecipesService {
     {
       id:'r1',
       title: 'Boutique cosmétique de Hassan',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg',
+      imageUrl: 'assets/pictures/boutique_cosmetique.jpg',
       ingredients: ['Défrisons', 'Crèmes', 'Parfums pour homme et femme'],
       productType :'Articles que vous pouvez trouver dans cette boutique :'
     
@@ -17,7 +17,7 @@ export class RecipesService {
     {
       id:'r2',
       title: 'Boutique consmétique de Amadou',
-      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX5i-w65JTrM2H1tclmo0_Di-G8cKBaIsCAg&usqp=CAU',
+      imageUrl: 'assets/pictures/boutique_cosmetique1.jpg',
       ingredients: ['Savons de douches', 'Chaines complée', 'Bracelets'],
       productType :'Articles que vous pouvez trouver dans cette boutique :'
     
